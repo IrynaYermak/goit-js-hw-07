3b13413f7c067f1832c312fa6572a977ca3d419a
Iryna Yermak <1991irenka@gmail.com>
2025-08-23 22:31:38 +0100

    made corrections

--- js/task-4.js
+++ js/task-4.js
@@ -7,13 +7,19 @@ function hendlerSubmit(event) {
 
   const elements = event.target.elements;
 
-  const info = {
-    email: elements.email.value.trim(),
-    pass: elements.password.value.trim(),
-  };
-  if (info.email === "" || info.pass === "") {
+  const email = elements.email.value.trim();
+  const pass = elements.password.value.trim();
+
+  if (email === "" || pass === "") {
     alert("All form fields must be filled in");
+    event.target.reset();
+    return;
   }
+
+  const info = {
+    email,
+    pass,
+  };
 
   console.log(info);
   event.target.reset();
