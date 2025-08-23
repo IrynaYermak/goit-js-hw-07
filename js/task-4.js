ef05eb325a7a259fdc96b08a85132d86fb0f356c
Iryna Yermak <1991irenka@gmail.com>
2025-08-23 21:23:49 +0100

    made task 4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,20 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", hendlerSubmit);
+
+function hendlerSubmit(event) {
+  event.preventDefault();
+
+  const elements = event.target.elements;
+
+  const info = {
+    email: elements.email.value.trim(),
+    pass: elements.password.value.trim(),
+  };
+  if (info.email === "" || info.pass === "") {
+    alert("All form fields must be filled in");
+  }
+
+  console.log(info);
+  event.target.reset();
+}
